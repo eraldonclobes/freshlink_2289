344e10ffa9ce58462abd5ae7200e96449607780d
eraldonclobes <adrian.murilo95@gmail.com>
2025-08-30 13:19:57 -0300

    Updated BulkActions.jsx

--- src/pages/product-management/components/BulkActions.jsx
+++ src/pages/product-management/components/BulkActions.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Icon from '../../../components/AppIcon';
 import Button from '../../../components/ui/Button';
-import Input from '../../../components/ui/Input';
+import { Input } from '../../../components/ui/Input';
 import Select from '../../../components/ui/Select';
 
 const BulkActions = ({ selectedProducts, onBulkUpdate, onClearSelection }) => {
