831f3ba9d07c7fa1a80077af06e30f3cfbb60aa4
eraldonclobes <adrian.murilo95@gmail.com>
2025-08-30 13:29:24 -0300

    Updated ProductCard.jsx

--- src/pages/product-management/components/ProductCard.jsx
+++ src/pages/product-management/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Icon from '../../../components/AppIcon';
 import Image from '../../../components/AppImage';
-import Button from '../../../components/ui/Button';
+import Button from '../../../components/ui/button';
 
 const ProductCard = ({ 
   product, 
