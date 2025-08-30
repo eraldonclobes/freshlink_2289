25e99fd5fb4c4fec995e7018691f5a775fd17124
eraldonclobes <adrian.murilo95@gmail.com>
2025-08-30 13:12:14 -0300

    Updated AccountStep.jsx

--- src/pages/vendor-registration/components/AccountStep.jsx
+++ src/pages/vendor-registration/components/AccountStep.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Input } from '../../../components/ui/input';
-import { Button } from '../../../components/ui/button';
+import { Button } from '../../../components/ui/Button';
 import { Checkbox } from '../../../components/ui/checkbox';
 import { Label } from '../../../components/ui/label';
 import Icon from '../../../components/AppIcon';
