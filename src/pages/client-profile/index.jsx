d5e4c27319c83bc7bb8a49e32beea7ffe461ca8b
eraldonclobes <adrian.murilo95@gmail.com>
2025-08-30 13:23:49 -0300

    Updated index.jsx

--- src/pages/client-profile/index.jsx
+++ src/pages/client-profile/index.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import ResponsiveHeader from '../../components/ui/ResponsiveHeader';
 import Footer from '../../components/ui/Footer';
 import Button from '../../components/ui/Button';
-import Input from '../../components/ui/Input';
+import Input from '../../components/ui/input';
 import Icon from '../../components/AppIcon';
 import Image from '../../components/AppImage';
 
