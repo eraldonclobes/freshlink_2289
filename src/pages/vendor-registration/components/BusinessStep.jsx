882c9fe5a819151df2ef1b7aa2667062e0479ff1
eraldonclobes <adrian.murilo95@gmail.com>
2025-08-30 13:12:29 -0300

    Updated BusinessStep.jsx

--- src/pages/vendor-registration/components/BusinessStep.jsx
+++ src/pages/vendor-registration/components/BusinessStep.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Input } from '../../../components/ui/input';
-import { Button } from '../../../components/ui/button';
+import { Button } from '../../../components/ui/Button';
 import { Label } from '../../../components/ui/label';
 import { Textarea } from '../../../components/ui/textarea';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../../../components/ui/select';
