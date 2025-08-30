5445c6d8b582d2d529e34bfb4e953c39dfbd430d
eraldonclobes <adrian.murilo95@gmail.com>
2025-08-30 13:12:39 -0300

    Updated VendorHeroSection.jsx

--- src/pages/vendor-profile-products/components/VendorHeroSection.jsx
+++ src/pages/vendor-profile-products/components/VendorHeroSection.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Icon from '../../../components/AppIcon';
 import Image from '../../../components/AppImage';
-import Button from '../../../components/ui/button';
+import { Button } from '../../../components/ui/Button';
 
 const VendorHeroSection = ({ vendor, onWhatsAppContact, onDirections, onShare }) => {
     const [isDescriptionExpanded, setIsDescriptionExpanded] = React.useState(false);
