b25383a99bdaf87d1cb3d4ce58d288402a5a0bf3
eraldonclobes <adrian.murilo95@gmail.com>
2025-08-30 13:12:07 -0300

    Updated LocationStep.jsx

--- src/pages/vendor-registration/components/LocationStep.jsx
+++ src/pages/vendor-registration/components/LocationStep.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Input } from '../../../components/ui/input';
-import { Button } from '../../../components/ui/button';
+import { Button } from '../../../components/ui/Button';
 import { Label } from '../../../components/ui/label';
 import Icon from '../../../components/AppIcon';
 
