675afc07c0b927090aa93e0efb69a860233cc46c
eraldonclobes <adrian.murilo95@gmail.com>
2025-08-30 13:29:29 -0300

    Updated ProductFilters.jsx

--- src/pages/product-management/components/ProductFilters.jsx
+++ src/pages/product-management/components/ProductFilters.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Icon from '../../../components/AppIcon';
-import Button from '../../../components/ui/Button';
+import Button from '../../../components/ui/button';
 
 import Select from '../../../components/ui/select';
 
